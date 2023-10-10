89114dfcac99617b70efea9891d04fff2f44763f
Rohan-IIT <rpawar3@hawk.iit.edu>
2023-10-09 20:54:14 -0500

    Css Minified

--- public/javascripts/mdb.js
+++ public/javascripts/mdb.js
@@ -1,28 +1,4 @@
-/*!
- * Material Design for Bootstrap 4
- *   Version: MDB FREE 4.19.2
- * 
- * 
- *   Copyright: Material Design for Bootstrap
- *   https://mdbootstrap.com/
- * 
- *   Read the license: https://mdbootstrap.com/general/license/
- * 
- * 
- *   Documentation: https://mdbootstrap.com/
- * 
- *   Getting started: https://mdbootstrap.com/docs/jquery/getting-started/download/
- * 
- *   Tutorials: https://mdbootstrap.com/education/bootstrap/
- * 
- *   Templates: https://mdbootstrap.com/templates/
- * 
- *   Support: https://mdbootstrap.com/support/
- * 
- *   Contact: office@mdbootstrap.com
- * 
- *   Attribution: Animate CSS, Twitter Bootstrap, Materialize CSS, Normalize CSS, Waves JS, WOW JS, Toastr, Chart.js, jquery.easing.js, velocity.min.js, chart.js, wow.js, scrolling-navbar.js, waves.js, forms-free.js, enhanced-modals.js, treeview.js
- */
+
 ! function(t) {
     var e = {};
 
